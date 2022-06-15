e633baddf3172116766a4e9f4a39791b99eb991f
ivan2282ofiical <90458162+ivan2282ofiical@users.noreply.github.com>
2022-06-15 16:44:30 +0300

    Update Install.lua

--- Installer/Install.lua
+++ Installer/Install.lua
@@ -13,7 +13,7 @@ local function drawmenu()
    term.clear()
    term.setCursorPos(2,2)
    term.write("INSTALL IMPERY OS")
-   term.setCursorPos(w-11.1)
+   term.setCursorPos(11.1)
    if noption == 1 then
        term.write("Programs")
    elseif noption == 2 then
